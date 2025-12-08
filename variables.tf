a78e4f5bc396d81a9160f4d6297dc27cf16c31e9
Erica Pescio <edro15@users.noreply.github.com>
2025-12-08 17:15:56 +0100

    docs:added variables descriptions

--- variables.tf
+++ variables.tf
@@ -4,35 +4,39 @@
 
 variable "vpc_id" {
   default     = "vpc-2c5fa94b"
-  description = "VPC ID"
+  description = "VPC ID."
 }
 
 variable "subnet_id" {
   default     = "subnet-fdc73bb4"
-  description = "Subnet ID"
+  description = "Subnet ID."
 }
 
 variable "aws_account_region" {
-  description = "Provide the desired region"
+  description = "Provide the desired AWS region (e.g. us-east-1, etc)."
   default     = "eu-west-1"
 }
 
 variable "instance_type" {
+  description = "Provide the instance type (e.g. t3.small, etc)."
   type    = string
   default = "t3.large"
 }
 
 variable "splunk_version" {
+  description = "Provide Splunk version."
   type    = string
   default = "9.4.4"
 }
 
 variable "ticket_id" {
+  description = "Provide an ID / name to identify the security group and the EC2."
   type    = string
   default = "fdse-3070"
 }
 
 variable "key_name" {
+  description = "Provide the name of the AWS public key."
   type    = string
   default = "erica-k"
 }
